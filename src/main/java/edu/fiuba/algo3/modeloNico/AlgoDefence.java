3ff84f08ccdea41c7591196e850aebe30126d46e
nllosas <nllosas@fi.uba.ar>
2023-06-07 18:12:55 -0300

    AlgoDefence instancia un Juego

--- src/main/java/edu/fiuba/algo3/modeloNico/AlgoDefence.java
+++ src/main/java/edu/fiuba/algo3/modeloNico/AlgoDefence.java
@@ -1,4 +1,14 @@
 package edu.fiuba.algo3.modeloNico;
 
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+
 public class AlgoDefence {
+
+    Juego juego;
+
+    public AlgoDefence() throws IOException, ParseException {
+        this.juego = new Juego();
+    }
 }
