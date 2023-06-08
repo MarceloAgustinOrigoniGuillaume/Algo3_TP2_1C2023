880eeece2829c77cbae9194de0db0a5eb4e1b588
Tomás Exner <texner@fi.uba.ar>
2023-06-08 16:28:53 -0300

    Se magrega clase jugadortest

--- src/main/test/edu/fiuba/algo3/entrega2/TestsIntegracion.java
+++ src/main/test/edu/fiuba/algo3/entrega2/TestsIntegracion.java
@@ -24,43 +24,8 @@ public class TestsIntegracion {
     }
 
 
-    public static class jugadorTest {
-        @Test
-        public void verificarQueJugadorEmpiezaConVidaYCreditosCorrespondientes() {
-            Jugador jugador = new Jugador();
-            assertEquals(100,jugador.obtenerCreditos());
-            assertEquals(20,jugador.obtenerVida());
-        }
-
-        @Test
-        public void verificarQueSeDispongaDeCreditoParaRealizarConstrucciones() {
-            Jugador jugador = new Jugador();
-            TorreBlanca torreBlanca = new TorreBlanca();
-            assertEquals(jugador.puedeCostear(torreBlanca),true);
-        }
-        @Test
-        public void verificarQueNoSeDispongaDeCreditoParaRealizarConstrucciones() {
-            Jugador jugador = new Jugador();
-            TorreBlanca torreBlanca = new TorreBlanca();
-
-            for (int i = 0; i < 10;i++){
-                assertEquals(jugador.puedeCostear(torreBlanca),true);
-                jugador.costear(torreBlanca);
-            }
-            assertEquals(jugador.puedeCostear(torreBlanca),false);
-        }
-
-        @Test
-        public void verificarQueJugadorGaneCreditosCorrespondientesAlMatarSpider() {
-            Jugador jugador = new Jugador();
-
-            jugador.ganoCreditos(10);
-
-            assertTrue(jugador.obtenerCreditos() > 100);
-            assertTrue(jugador.obtenerCreditos() <=110);
-
-        }
 
 
-    }
+
+
 }
