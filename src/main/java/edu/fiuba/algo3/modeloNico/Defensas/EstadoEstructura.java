1785a065ac47a9c65f55c8ac0719c35fd8f4d491
isaiascabreraa <isaiascabrera20@gmail.com>
2023-06-08 01:54:02 -0300

    Agregue cosas a Defensas. El patron Strategy

---
+++ src/main/java/edu/fiuba/algo3/modeloNico/Defensas/EstadoEstructura.java
@@ -0,0 +1,11 @@
+package edu.fiuba.algo3.modeloNico.Defensas;
+
+import edu.fiuba.algo3.modeloNico.Mapa.Mapa;
+
+public interface EstadoEstructura {
+
+    public void ejecutarMetodo(Defensa defensa, Mapa mapa);
+
+    //Hace lo que se espera que haga la instancia de EstadoConstruido o EstadoContruyendo.
+
+}
