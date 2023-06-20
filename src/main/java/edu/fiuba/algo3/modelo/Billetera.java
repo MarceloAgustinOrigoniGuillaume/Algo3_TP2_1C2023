8c7c12ab69302a3400f905f4cc3fe6fb2f6001de
nllosas <nllosas@fi.uba.ar>
2023-06-20 13:59:06 -0300

    pequenia modificacion al sistema Billetera con respecto a las hormigas

--- src/main/java/edu/fiuba/algo3/modelo/Billetera.java
+++ src/main/java/edu/fiuba/algo3/modelo/Billetera.java
@@ -2,13 +2,21 @@ package edu.fiuba.algo3.modelo;
 
 public final class Billetera {
     private static Billetera billetera;
+
+    private int hormigasMatadas;
     private int creditos;
     private Billetera(){
         this.creditos = 0;
+        this.hormigasMatadas = 0;
     }
 
     public void agregarCreditos(int creditosAgregados){
         creditos += creditosAgregados;
+    }
+
+    public int agregarHormigaMatada() {
+        hormigasMatadas++;
+        return hormigasMatadas;
     }
 
     public void reducirCreditos(int creidosReducidos){
