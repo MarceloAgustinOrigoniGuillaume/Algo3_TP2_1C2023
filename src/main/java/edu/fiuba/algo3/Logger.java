9d844db57462e61df868213b1e867d6347eaf90a
MarceloAgustinOrigoniGuillaume <marce.origoni@gmail.com>
2023-06-27 15:16:37 -0300

    cambio en el logger para filtrar mas facil

--- src/main/java/edu/fiuba/algo3/Logger.java
+++ src/main/java/edu/fiuba/algo3/Logger.java
@@ -1,44 +1,109 @@
 package edu.fiuba.algo3;
 
-    public class Logger {
+import java.util.Hashtable;
 
-        private static final Logger unicaInstancia = new Logger();
+public class Logger {
 
-        private static boolean loggerActivado = true;
-        private static boolean loggerTests = true;
+    private static final Logger unicaInstancia = new Logger();
 
-        private Logger() {
-        }
+    private static boolean loggerActivado = true;
+    
+    private Hashtable<String,Boolean> identifiers;
 
-        public void setTestsMode(boolean activo){
-            loggerTests = activo;
-        }
+    // para multiples parametros, los separas por espacio.
+    private static final String JOINT= " ";
 
-        public static void setTestMode(boolean activo){
-            Logger.obtener().setTestsMode(activo);       
-        }
+    // id logs
+    public static final String INFO= "info";
+    public static final String ERROR= "error";
+    public static final String DEBUG= "debug";
+    public static final String GENERAL= "general";
 
-        public static Logger obtener() {
-            return unicaInstancia;
-        }
+    private Logger() {
 
-        public static void info(String unString){
-            if(loggerTests){
-                Logger.obtener().log(unString);
+        identifiers = new Hashtable<String,Boolean>();
+
+        // info, son logs generales de acciones o cosas
+        // pero sin entrar en detalles. 
+        setIdentifierActive(INFO,true);
+
+        // errores
+        setIdentifierActive(ERROR,true);
+        
+        // general, cualquier log que no sea otro identifier entra aca
+        setIdentifierActive(GENERAL,false);
+
+        // debug logs.
+        setIdentifierActive(DEBUG,false);
+    }
+
+    public void setIdentifierActive(String identifier, boolean activo){
+        identifiers.put(identifier,activo);
+    }
+
+    public static void setActive(String identifier, boolean activo){
+        Logger.obtener().setIdentifierActive(identifier,activo); 
+    }
+
+    public static Logger obtener() {
+        return unicaInstancia;
+    }
+
+
+
+    public void toggle(boolean activado){
+        loggerActivado = activado;
+    }
+
+
+
+    // funcion log 
+
+
+    public void logParams(String identifier, Object ... objetosLogeados) {
+        log(identifier, objetosLogeados);
+    }
+
+    public void log(String identifier, Object[] objetosLogeados){
+        if (loggerActivado){
+            if(!identifiers.containsKey(identifier)){
+                identifier = GENERAL;
             }
-        }
 
-        public void toggle(boolean bool){
-            loggerActivado = bool;
-        }
+            if(!identifiers.get(identifier)){
+                return;// no esta activo.
+            }
 
-        public void log(String mensajeLogeado) {
-            if (loggerActivado)
-                System.out.println(mensajeLogeado);
-        }
+            String mensajeLogeado = objetosLogeados[0].toString();
 
-        public static void Log(String unString) {
-            Logger.obtener().log(unString);
+            for(int i = 1; i<objetosLogeados.length; i++){
+                mensajeLogeado+= JOINT+objetosLogeados[i].toString();
+            }
+
+
+            System.out.println(identifier+">"+mensajeLogeado);
         }
 
     }
+
+    
+
+    // static loggers
+    public static void info(Object ... objetosLogeados){
+        Logger.obtener().log(INFO,objetosLogeados);
+    }
+
+    public static void Log(Object ... objetosLogeados) {
+        Logger.obtener().log(GENERAL,objetosLogeados);
+    }
+
+    public static void err(Object ... objetosLogeados) {
+        Logger.obtener().log(ERROR,objetosLogeados);
+    }
+
+    public static void dbg(Object ... objetosLogeados) {
+        Logger.obtener().log(DEBUG,objetosLogeados);
+    }
+
+
+}
